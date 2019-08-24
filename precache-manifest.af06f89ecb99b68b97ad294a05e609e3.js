0636b8a528222117da662adfaad1a83eeff2ef1e
Daniil <dkurpachenko@gmail.com>
2019-08-24 17:37:47 +0300

    Updates

---
+++ precache-manifest.af06f89ecb99b68b97ad294a05e609e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "19fe6c3119481b52c820",
+    "url": "./static/js/main.19fe6c31.chunk.js"
+  },
+  {
+    "revision": "1416a59004992e3d8fcb",
+    "url": "./static/js/2.1416a590.chunk.js"
+  },
+  {
+    "revision": "19fe6c3119481b52c820",
+    "url": "./static/css/main.92cf65e9.chunk.css"
+  },
+  {
+    "revision": "1416a59004992e3d8fcb",
+    "url": "./static/css/2.80aed42c.chunk.css"
+  },
+  {
+    "revision": "5f34514032ee921ecda99631b05a84e3",
+    "url": "./index.html"
+  }
+];
